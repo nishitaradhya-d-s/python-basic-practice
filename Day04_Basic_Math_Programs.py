b7f1a2d8adfa00ef9a11def050cd84e170ca0080
Nishitaradhya D S <nishitaradhya@gmail.com>
2026-02-22 14:29:18 +0530

    Day04_Basic_Math_Programs.py
    created swap two numbers through user input, square and cube of a number , area of circle programs

---
+++ Day04_Basic_Math_Programs.py
@@ -0,0 +1,19 @@
+# Day 4 - Basic Mathematical Programs
+
+# Swapping logic demonstration
+a=int(input("enter a value: "))
+b=int(input("enter b value: "))
+print("a=", a)
+print("b=", b)
+print("a=",a+b-a)
+print("b=",b+a-b)
+
+# Square and cube of a number
+x=int(input("enter x number: "))
+print(x*x)
+print(x*x*x)
+
+# Area of circle using formula πr²
+r=5
+area= 3.142*(r**2) # using power operator **
+print(area)
