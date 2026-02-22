fd780ee44acfe184d257196d5c37b007865e02ed
Nishitaradhya D S <nishitaradhya@gmail.com>
2026-02-22 14:22:12 +0530

    Day01_Variables_and_Types.py
    Practiced type conversion and arithmetic operations.

---
+++ Day01_Variables_and_Types.py
@@ -0,0 +1,52 @@
+# Day 1 - Variables and Data Types Practice
+
+# String to integer conversion
+age="100"
+print(age)
+print(type(age))
+age2=int(age)
+print(type(age2))
+
+# String variable
+name="nish"
+print(name)
+print(type(name))
+
+# Integer variable
+Age=22
+print(Age)
+print(type(Age))
+
+# Boolean variable
+am_i_student=True
+print(am_i_student)
+print(type(am_i_student))
+
+# Float variable
+height=5.4
+print(height)
+print(type(height))
+
+height2=int(height) # converting float to integer
+print(height2)
+
+# Adding converted age and integer Age
+print(int(age)+Age)
+
+# Arithmetic operations
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+# Expression evaluation
+print(a+b-a+b*2**3)
+
+#swapping 2 numbers
+print("a=",a+b-a)
+print("b=",b+a-b)
