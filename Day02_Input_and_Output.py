0879737dfe6435f49925d31d269197d61de171e3
Nishitaradhya D S <nishitaradhya@gmail.com>
2026-02-22 14:24:07 +0530

    Day02_Input_and_Output.py
    Input and Output
    learning user input program

---
+++ Day02_Input_and_Output.py
@@ -0,0 +1,11 @@
+# Day 2 - Input and String Concatenation
+
+name=input("enter your name: ")
+print(name)
+girl=input("enter a girl name:" )
+print(girl)
+boy=input("enter a boy namen:" )
+print(boy)
+
+# String concatenation
+print(girl +" loves "+ boy)
